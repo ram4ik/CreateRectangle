dab4bf63a3d9152e73e23de0ee2ae9e81b34e9df
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-03-08 21:55:10 +0200

    init

--- CreateRectangle/ContentView.swift
+++ CreateRectangle/ContentView.swift
@@ -9,8 +9,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var rect1 = false
+    @State private var rect2 = false
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Rectangle()
+                .fill(self.rect1 ? Color.black : Color.gray)
+                .frame(width: self.rect1 ? 100 : 50, height: self.rect1 ? 100 : 50)
+                .cornerRadius(self.rect1 ? 50 : 0)
+                .onTapGesture {
+                    self.rect1.toggle()
+            }.animation(.default)
+            
+            RoundedRectangle(cornerRadius: self.rect2 ? 0.0 : 15.0)
+                .stroke(self.rect2 ? Color.green : Color.red, lineWidth: self.rect2 ? 2.0 : 5.0)
+                .frame(width: self.rect2 ? 100 : 50, height: self.rect2 ? 100 : 50)
+                .onTapGesture {
+                    self.rect2.toggle()
+            }.animation(.default)
+        }
     }
 }
 
